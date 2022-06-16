d7da69e7c41873a8d4e6a75522d7c1d40134c85d
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-06-15 20:22:56 -0500

    feat(entity): added User entity
    User Collection, DTO, Repository and MapperUtil added

---
+++ src/main/java/com/santiagoposadag/cs50/receiverconsumer/collections/User.java
@@ -0,0 +1,20 @@
+package com.santiagoposadag.cs50.receiverconsumer.collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String Id;
+    private String name;
+    private String lastName;
+    private String routingKey;
+}
